b2d0c5b4e61f5f65f7467dba7144895f7c203cb7
Naoghuman <peter.rogge@yahoo.de>
2017-04-25 20:52:30 +0200

    Add basic sample classes for the library Lib-Tag.

---
+++ src/main/java/com/github/naoghuman/demo/libtag/libtagproject/usagetagrelationbuilder/UsageTagRelationBuilder.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2017 Naoghuman
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.naoghuman.demo.libtag.libtagproject.usagetagrelationbuilder;
+
+import com.github.naoghuman.demo.template.annotation.Project;
+import com.github.naoghuman.demo.template.annotation.Sample;
+
+/**
+ *
+ * @author Naoghuman
+ */
+@Sample(
+        /* cssURL = "[undefined]"; */
+        description = "This sample descripted the usage of the class com.github.naoghuman.lib.tag.core.TagRelationBuilder.", // NOI18N
+        /* javaDocURL = "[undefined]"; */
+        name = "06 Usage from the class TagRelationBuilder", // NOI18N
+        project = @Project(name = "Lib-Tag"), // NOI18N
+        /* sourceCodeURL = "[undefined]"; */
+        visible = true
+)
+public class UsageTagRelationBuilder {
+    
+}
