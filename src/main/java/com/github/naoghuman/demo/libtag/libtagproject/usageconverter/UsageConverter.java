42be23ba7bedb9a537be5352e78f08a3473dc3b6
Naoghuman <peter.rogge@yahoo.de>
2017-04-28 20:25:57 +0200

    Add new sample [UsageConverter] for the interface [Converter].

---
+++ src/main/java/com/github/naoghuman/demo/libtag/libtagproject/usageconverter/UsageConverter.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2017 Naoghuman
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.naoghuman.demo.libtag.libtagproject.usageconverter;
+
+import com.github.naoghuman.demo.template.annotation.Project;
+import com.github.naoghuman.demo.template.annotation.Sample;
+
+/**
+ *
+ * @author Naoghuman
+ */
+@Sample(
+        /* cssURL = "[undefined]"; */
+        description = "This sample descripted the usage of the interface com.github.naoghuman.lib.tag.core.Converter.", // NOI18N
+        /* javaDocURL = "[undefined]"; */
+        name = "08 Usage from the interface Converter", // NOI18N
+        /* overviewURL = "[undefined]", */
+        project = @Project(name = "Lib-Tag"), // NOI18N
+        /* sampleType = SampleType.NORMAL, */
+        /* sourceCodeURL = "[undefined]"; */
+        visible = true
+)
+public class UsageConverter {
+    
+}
